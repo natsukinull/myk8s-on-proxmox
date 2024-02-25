38025475b2b87d9bc880998d0f81440816810643
root <root@pve-node1.local>
2024-02-25 16:23:12 +0900

    test

---
+++ k8s-setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://github.com/kubespray/kubespray/archive/master.tar.gz
+tar -zxvf master.tar.gz 
---
+++ vm-setup.sh
@@ -0,0 +1,146 @@
+TARGET_BRANCH=$1
+TEMPLATE_VMID=5001
+CLOUDINIT_IMAGE_TARGET_VOLUME=CephPool01
+TEMPLATE_BOOT_IMAGE_TARGET_VOLUME=CephPool01
+BOOT_IMAGE_TARGET_VOLUME=CephPool01
+SNIPPET_TARGET_VOLUME=mycephfs
+SNIPPET_TARGET_PATH=/mnt/pve/${SNIPPET_TARGET_VOLUME}/snippets
+
+
+# # download the image(ubuntu 22.04 LTS)
+# wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
+
+# qm create $TEMPLATE_VMID --cores 2 --memory 4096 --net0 virtio,bridge=vmbr0 --name template-cp
+
+# # import the downloaded disk to $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME storage
+# qm importdisk $TEMPLATE_VMID jammy-server-cloudimg-amd64.img $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME
+
+# # finally attach the new disk to the VM as scsi drive
+# qm set $TEMPLATE_VMID --scsihw virtio-scsi-pci --scsi0 $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME:vm-$TEMPLATE_VMID-disk-0
+
+# # add Cloud-Init CD-ROM drive
+# qm set $TEMPLATE_VMID --ide2 $CLOUDINIT_IMAGE_TARGET_VOLUME:cloudinit
+
+# # set the bootdisk parameter to scsi0
+# qm set $TEMPLATE_VMID --boot c --bootdisk scsi0
+
+# # set serial console
+# qm set $TEMPLATE_VMID --serial0 socket --vga serial0
+
+# # migrate to template
+# qm template $TEMPLATE_VMID
+
+# # cleanup
+# rm jammy-server-cloudimg-amd64.img
+
+VM_LIST=(
+    # ---
+    # vmid:       proxmox上でVMを識別するID
+    # vmname:     proxmox上でVMを識別する名称およびホスト名
+    # cpu:        VMに割り当てるコア数(vCPU)
+    # mem:        VMに割り当てるメモリ(MB)
+    # vmsrvip:    VMのService Segment側NICに割り振る固定IP
+    # targetip:   VMの配置先となるProxmoxホストのIP
+    # targethost: VMの配置先となるProxmoxホストのホスト名
+    # ---
+    #vmid #vmname      #cpu #mem  #vmsrvip    #targetip    #targethost
+    "1001 master01  2    2048  192.168.100.220  192.168.100.90 pve-node3"
+    # "1002 master02  2    2048  192.168.100.221  192.168.100.90 pve-node3"
+    # "1003 master03  2    2048  192.168.100.222  192.168.100.90 pve-node3"
+    # "1004 worker01  2    2048  192.168.100.223  192.168.100.90 pve-node3"
+    # "1005 worker02  2    2048  192.168.100.224  192.168.100.90 pve-node3"
+    # "1006 worker03  2    2048  192.168.100.225  192.168.100.90 pve-node3"
+    # "1007 storage01 2    2048  192.168.100.226  192.168.100.90 pve-node3"
+    # "1008 storage02 2    2048  192.168.100.227  192.168.100.90 pve-node3"
+    # "1009 storage03 2    2048  192.168.100.228  192.168.100.90 pve-node3"
+    # "1010 access01  2    2048  192.168.100.229  192.168.100.90 pve-node3"
+)
+
+for array in "${VM_LIST[@]}"
+do
+    echo "${array}" | while read -r vmid vmname cpu mem vmsrvip targetip targethost
+    do
+        # ここで何らかの処理を行う。例えば変数を出力する。
+        echo "VM ID: ${vmid}, Name: ${vmname}, CPU: ${cpu}, Memory: ${mem}MB, Service IP: ${vmsrvip}, Target IP: ${targetip}, Host: ${targethost}"
+
+                # clone from template
+        # in clone phase, can't create vm-disk to local volume
+        qm clone "${TEMPLATE_VMID}" "${vmid}" --name "${vmname}" --full true --target "${targethost}"
+        
+        # set compute resources
+        ssh -n "${targetip}" qm set "${vmid}" --cores "${cpu}" --memory "${mem}"
+
+        # move vm-disk to local
+        ssh -n "${targetip}" qm move-disk "${vmid}" scsi0 "${BOOT_IMAGE_TARGET_VOLUME}" --delete true
+
+        # resize disk (Resize after cloning, because it takes time to clone a large disk)
+        ssh -n "${targetip}" qm resize "${vmid}" scsi0 30G
+
+# ----- #
+cat > "$SNIPPET_TARGET_PATH"/"$vmname"-user.yaml << EOF
+#cloud-config
+hostname: ${vmname}
+timezone: Asia/Tokyo
+manage_etc_hosts: true
+chpasswd:
+  expire: False
+users:
+  - default
+  - name: cloudinit
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    lock_passwd: false
+    # mkpasswd --method=SHA-512 --rounds=4096
+    # password is zaq12wsx
+    passwd: \$6\$rounds=4096\$Xlyxul70asLm\$9tKm.0po4ZE7vgqc.grptZzUU9906z/.vjwcqz/WYVtTwc5i2DWfjVpXb8HBtoVfvSY61rvrs/iwHxREKl3f20
+ssh_pwauth: true
+ssh_authorized_keys: []
+package_upgrade: true
+runcmd:
+  # set ssh_authorized_keys
+  - su - cloudinit -c "mkdir -p ~/.ssh && chmod 700 ~/.ssh"
+  - su - cloudinit -c "curl -sS https://github.com/natsukinull.keys >> ~/.ssh/authorized_keys"
+  - su - cloudinit -c "chmod 600 ~/.ssh/authorized_keys"
+  # run install scripts
+  # change default shell to bash
+  - chsh -s $(which bash) cloudinit
+EOF
+# ----- #
+        # END irregular indent because heredoc
+
+        # create snippet for cloud-init(network-config)
+        # START irregular indent because heredoc
+# ----- #
+cat > "$SNIPPET_TARGET_PATH"/"$vmname"-network.yaml << EOF
+version: 1
+config:
+  - type: physical
+    name: ens18
+    subnets:
+    - type: static
+      address: '${vmsrvip}'
+      netmask: '255.255.255.0'
+      gateway: '192.168.100.1'
+  - type: nameserver
+    address:
+    - '8.8.8.8'
+    search:
+    - 'local'
+EOF
+# ----- #
+        # END irregular indent because heredoc
+
+        # set snippet to vm
+        ssh -n "${targetip}" qm set "${vmid}" --cicustom "user=${SNIPPET_TARGET_VOLUME}:snippets/${vmname}-user.yaml,network=${SNIPPET_TARGET_VOLUME}:snippets/${vmname}-network.yaml"
+
+    done
+done
+
+for array in "${VM_LIST[@]}"
+do
+    echo "${array}" | while read -r vmid vmname cpu mem vmsrvip targetip targethost
+    do
+        # start vm
+        ssh -n "${targetip}" qm start "${vmid}"
+        
+    done
+done
