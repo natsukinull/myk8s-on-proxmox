e86c248fbbff6b972adfe15e0b97da8adb89d55b
root <root@pve-node1.local>
2024-02-25 18:43:05 +0900

    微修正

--- k8s-setup.sh
+++ k8s-setup.sh
@@ -16,3 +16,5 @@ sudo chown -R cloudinit:cloudinit ~/kubespray-master/
 sudo apt install -y python3-pip
 sudo pip install -r ~/kubespray-master/requirements.txt 
 sudo apt install -y sshpass
+ansible-galaxy collection install ansible.posix
+
