e1f22ab8740dc07eba65eb6ee93a4de635cf6f5f
natsukinull <naru.06ait@gmail.com>
2024-03-04 12:08:05 +0900

    test

--- k8s-setup.sh
+++ k8s-setup.sh
@@ -13,31 +13,30 @@ esac
 
 
 sudo apt install -y unzip
-wget https://github.com/kubernetes-sigs/kubespray/archive/refs/heads/release-2.24.zip
-unzip /home/cloudinit/release-2.24.zip
+cd /home/cloudinit
+wget https://github.com/kubernetes-sigs/kubespray/archive/v2.12.3.tar.gz
+tar xvf v2.12.3.tar.gz 
 
-sudo chown -R cloudinit:cloudinit /home/cloudinit/kubespray-release-2.24/
+sudo chown -R cloudinit:cloudinit /home/cloudinit/kubespray-2.12.3/
 sudo apt install -y python3-pip
-sudo pip install -r /home/cloudinit/kubespray-release-2.24/requirements.txt 
+sudo pip install -r /home/cloudinit/kubespray-2.12.3/requirements.txt 
 sudo apt install -y sshpass
 ansible-galaxy collection install ansible.posix
 
-cp -rfp /home/cloudinit/kubespray-release-2.24/inventory/sample /home/cloudinit/kubespray-release-2.24/inventory/mycluster
-cp -p /home/cloudinit/kubespray-release-2.24/inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml /home/cloudinit/kubespray-release-2.24/inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml.bk
-sed 's/^kube_version: v.*/kube_version: v1.27.0/' /home/cloudinit/kubespray-release-2.24/inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml | sudo tee /home/cloudinit/kubespray-release-2.24/inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
+cp -rfp /home/cloudinit/kubespray-2.12.3/inventory/sample /home/cloudinit/kubespray-2.12.3/inventory/mycluster
+cp -p /home/cloudinit/kubespray-2.12.3/inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml /home/cloudinit/kubespray-2.12.3/inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml.bk
+# sed 's/^kube_version: v.*/kube_version: v1.27.0/' /home/cloudinit/kubespray-2.12.3/inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml | sudo tee /home/cloudinit/kubespray-2.12.3/inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
 
-cat <<EOF | tee /home/cloudinit/kubespray-release-2.24/inventory/mycluster/inventory.ini 
+cat <<EOF | tee /home/cloudinit/kubespray-2.12.3/inventory/mycluster/inventory.ini 
 [all]
-master01 ansible_host=192.168.240.220  
-master02 ansible_host=192.168.240.221  
-master03 ansible_host=192.168.240.222
-worker01 ansible_host=192.168.240.223
-worker02 ansible_host=192.168.240.224
-worker03 ansible_host=192.168.240.225
-storage01 ansible_host=192.168.240.226
-storage02 ansible_host=192.168.240.227
-storage03 ansible_host=192.168.240.228
-access01 ansible_host=192.168.240.229
+api1     ansible_host=192.168.240.240
+api2     ansible_host=192.168.240.241
+gpu1     ansible_host=192.168.240.242
+# gpu2     ansible_host=192.168.240.243
+# storage1 ansible_host=192.168.240.244
+# storage2 ansible_host=192.168.240.245
+# storage3 ansible_host=192.168.240.246
+# access1  ansible_host=192.168.240.247
 
 [all:vars]
 ansible_ssh_port=22
@@ -45,23 +44,19 @@ ansible_ssh_user=cloudinit
 ansible_ssh_pass=zaq12wsx
 
 [kube-master]
-master01
-master02
-master03
+api1
+api2
+gpu1
 
 [etcd]
-master01
-master02
-master03
+api1
+api2
+gpu1
 
 [kube-node]
-worker01
-worker02
-worker03
-storage01
-storage02
-storage03
-access01
+api1
+api2
+gpu1
 
 [calico-rr]
 
@@ -82,29 +77,24 @@ cat <<EOF | tee /root/.ansible.cfg
 ssh_args = -o ControlMaster=auto -o ControlPersist=60s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
 EOF
 
-wget https://github.com/natsukinull/myk8s-on-proxmox/archive/refs/heads/stg.zip
-unzip /home/cloudinit/stg.zip
-ansible-playbook -i /home/cloudinit/myk8s-on-proxmox-stg/ansible/hosts/inventory.ini /home/cloudinit/myk8s-on-proxmox-stg/ansible/playbook_preset_kubespray.yaml
+wget https://github.com/natsukinull/myk8s-on-proxmox/archive/refs/heads/develop.zip
+unzip /home/cloudinit/develop.zip
+ansible-playbook -i /home/cloudinit/myk8s-on-proxmox-develop/ansible/hosts/inventory.ini /home/cloudinit/myk8s-on-proxmox-develop/ansible/playbook_preset_kubespray.yaml
 
-cd /home/cloudinit/kubespray-release-2.24
-ansible-playbook -i ./inventory/mycluster/inventory.ini ./cluster.yml -vvv -b
+# cd /home/cloudinit/kubespray-2.12.3
+# ansible-playbook -i ./inventory/mycluster/inventory.ini ./cluster.yml -vvv -b
 
-sudo chown -R cloudinit:cloudinit /home/cloudinit/.ssh/
+# sudo chown -R cloudinit:cloudinit /home/cloudinit/.ssh/
 
-mkdir /home/cloudinit/.kube
-sudo chown -R cloudinit:cloudinit /home/cloudinit/.kube
-sudo cp -p /etc/kubernetes/admin.conf /home/cloudinit/.kube/config
-sudo chown cloudinit:cloudinit /home/cloudinit/.kube/config
-sudo cp -p /etc/kubernetes/admin.conf /root/.kube/config
+# mkdir /home/cloudinit/.kube
+# sudo chown -R cloudinit:cloudinit /home/cloudinit/.kube
+# sudo cp -p /etc/kubernetes/admin.conf /home/cloudinit/.kube/config
+# sudo chown cloudinit:cloudinit /home/cloudinit/.kube/config
+# sudo cp -p /etc/kubernetes/admin.conf /root/.kube/config
 
-ssh cloudinit@192.168.240.221 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_ed25519 mkdir ~/.kube
-ssh cloudinit@192.168.240.221 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_ed25519 sudo cp -p /etc/kubernetes/admin.conf $HOME/.kube/config
-ssh cloudinit@192.168.240.221 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_ed25519 sudo chown cloudinit:cloudinit /home/cloudinit/.kube/config
-ssh cloudinit@192.168.240.221 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_ed25519 sudo cp -p /etc/kubernetes/admin.conf /root/.kube/config
+# ssh cloudinit@192.168.240.241 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_ed25519 mkdir ~/.kube
+# ssh cloudinit@192.168.240.241 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_ed25519 sudo cp -p /etc/kubernetes/admin.conf $HOME/.kube/config
+# ssh cloudinit@192.168.240.241 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_ed25519 sudo chown cloudinit:cloudinit /home/cloudinit/.kube/config
+# ssh cloudinit@192.168.240.241 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_ed25519 sudo cp -p /etc/kubernetes/admin.conf /root/.kube/config
 
-ssh cloudinit@192.168.240.222 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_ed25519 mkdir ~/.kube
-ssh cloudinit@192.168.240.222 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_ed25519 sudo cp -p /etc/kubernetes/admin.conf $HOME/.kube/config
-ssh cloudinit@192.168.240.222 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_ed25519 sudo chown cloudinit:cloudinit /home/cloudinit/.kube/config
-ssh cloudinit@192.168.240.222 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_ed25519 sudo cp -p /etc/kubernetes/admin.conf /root/.kube/config
-
-sudo reboot
+# sudo reboot
--- vm-setup.sh
+++ vm-setup.sh
@@ -1,5 +1,5 @@
 TARGET_BRANCH=$1
-TEMPLATE_VMID=4999
+TEMPLATE_VMID=4998
 # CLOUDINIT_IMAGE_TARGET_VOLUME=CephPool01
 CLOUDINIT_IMAGE_TARGET_VOLUME=local-lvm
 # TEMPLATE_BOOT_IMAGE_TARGET_VOLUME=CephPool01
@@ -12,12 +12,15 @@ SNIPPET_TARGET_PATH=/var/lib/vz/snippets
 REPOSITORY_RAW_SOURCE_URL="https://raw.githubusercontent.com/natsukinull/myk8s-on-proxmox/${TARGET_BRANCH}"
 
 # download the image(ubuntu 22.04 LTS)
-wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
+# wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
 
-qm create $TEMPLATE_VMID --cores 2 --memory 4096 --net0 virtio,bridge=vmbr1 --name template-cp
+# download the image(ubuntu 18.04 LTS)
+wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img
+
+qm create $TEMPLATE_VMID --cores 2 --memory 2048 --net0 virtio,bridge=vmbr1 --name template-cp-ubuntu18
 
 # import the downloaded disk to $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME storage
-qm importdisk $TEMPLATE_VMID jammy-server-cloudimg-amd64.img $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME
+qm importdisk $TEMPLATE_VMID bionic-server-cloudimg-amd64.img $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME
 
 # finally attach the new disk to the VM as scsi drive
 qm set $TEMPLATE_VMID --scsihw virtio-scsi-pci --scsi0 $TEMPLATE_BOOT_IMAGE_TARGET_VOLUME:vm-$TEMPLATE_VMID-disk-0
@@ -35,7 +38,7 @@ qm set $TEMPLATE_VMID --serial0 socket --vga serial0 --agent enabled=1
 qm template $TEMPLATE_VMID
 
 # cleanup
-rm jammy-server-cloudimg-amd64.img
+rm bionic-server-cloudimg-amd64.img
 
 VM_LIST=(
     # ---
@@ -48,16 +51,14 @@ VM_LIST=(
     # targethost: VMの配置先となるProxmoxホストのホスト名
     # ---
     #vmid #vmname      #cpu #mem  #vmsrvip    #targetip    #targethost
-    "5000 master01  8    4096  192.168.240.220  192.168.200.207 pve"
-    "5001 master02  2    2048  192.168.240.221  192.168.200.207 pve"
-    "5002 master03  2    2048  192.168.240.222  192.168.200.207 pve"
-    "5003 worker01  2    4096  192.168.240.223  192.168.200.207 pve"
-    "5004 worker02  2    2048  192.168.240.224  192.168.200.207 pve"
-    "5005 worker03  2    2048  192.168.240.225  192.168.200.207 pve"
-    "5006 storage01 2    2048  192.168.240.226  192.168.200.207 pve"
-    "5007 storage02 2    2048  192.168.240.227  192.168.200.207 pve"
-    "5008 storage03 2    2048  192.168.240.228  192.168.200.207 pve"
-    "5009 access01  2    2048  192.168.240.229  192.168.200.207 pve"
+    "6000 api1  4       4096  192.168.240.240  192.168.200.207 pve"
+    "6001 api2  2       2048  192.168.240.241  192.168.200.207 pve"
+    "6004 gpu1  2       2048  192.168.240.242  192.168.200.207 pve"
+    # "6005 gpu2  2       2048  192.168.240.243  192.168.200.207 pve"
+    # "6006 storage1 2    2048  192.168.240.244  192.168.200.207 pve"
+    # "6007 storage2 2    2048  192.168.240.245  192.168.200.207 pve"
+    # "6008 storage3 2    2048  192.168.240.246  192.168.200.207 pve"
+    # "6009 access1  2    2048  192.168.240.247  192.168.200.207 pve"
 )
 
 for array in "${VM_LIST[@]}"
